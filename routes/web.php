3911b92e24529bfbcf3021e870f101c96c61a129
clienit <onur.oktay@clienit.co.uk>
2020-07-30 11:04:48 +0100

    Remove unused home component

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,4 @@ Auth::routes(['register' => false]);
 Route::group(['middleware' => ['auth']], function () {
     Route::resource('companies', 'CompanyController');
 	Route::resource('employees', 'EmployeeController');
-
-	Route::get('/home', 'HomeController@index')->name('home');
 });
